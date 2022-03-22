56062cce4fa1542f82c809c642f453f77f5c5740
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-22 15:03:42 +0530

    add .

---
+++ UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using CabInvoiceGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CabInvoiceGeneratorUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
